81be986b3c7a41ef739a48bdb3e3f9832309b302
DeusEx01 <theonly.lightfall002@gmail.com>
2026-03-05 19:25:05 +0300

    refactor: remapped 'Harpoon file' to '<leader>a'

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,6 +3,14 @@ return {
     "folke/noice.nvim",
     opts = function(_, opts)
       opts.presets.lsp_doc_border = true
+      
     end,
-  }
+  },
+	{
+		"nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = false },
+		},
+	},
+
 }
