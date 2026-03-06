be266c1531687f8bef1a568ba5314ea968c12b7e
Semirai <theonly.lightfall002@gmail.com>
2026-03-06 20:34:47 +0300

    feat: added color highlighting via brenoprata10/nvim-highlight-colors

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,14 +1,38 @@
 return {
-  {
-    "folke/noice.nvim",
-    opts = function(_, opts)
-      opts.presets.lsp_doc_border = true
-      
-    end,
-  },
+	{
+		"folke/noice.nvim",
+		opts = function(_, opts)
+			opts.presets.lsp_doc_border = true
+		end,
+	},
 	{
 		"nvim-lspconfig",
 		opts = {
 			inlay_hints = { enabled = false },
 		},
-	}}
+
+		{
+			"brenoprata10/nvim-highlight-colors",
+			event = "BufReadPre",
+			opts = {
+				--'background'|'foreground'|'virtual'
+				render = "virtual",
+        virtual_symbol_position = "eow",
+
+				-- "●"|"♫"|"✦"|"▁▂▃▄▅▆▇█ █▇▆▅▄▃▂▁"|"◀■▶"|"●"
+				virtual_symbol = "●",
+				virtual_symbol_prefix = " ",
+				virtual_symbol_suffix = "",
+
+				enable_hex = true,
+				enable_short_hex = true,
+				enable_rgb = true,
+				enable_hsl = true,
+				enable_hsl_without_function = true,
+				enable_ansi = true,
+				enable_var_usage = true,
+				enable_tailwind = true,
+			},
+		},
+	},
+}
