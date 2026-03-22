82570e35730c91b35a865926dcc63eaf0e284538
Sem1ra1 <theonly.lightfall002@gmail.com>
2026-03-22 17:23:54 +0300

    refactor: replaced plugin stay-centered with native feature scrolloff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,4 +6,5 @@ vim.g.snacks_animate = false
 vim.g.autoformat = false
 vim.opt.guicursor = "n-v-c-i-sm:block,r-cr-o:hor20"
 vim.opt.iskeyword:append("-")
+vim.opt.scrolloff = 999  -- or any number you like
 
--- lua/plugins/stay-centered-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {   "arnamak/stay-centered.nvim",
-  lazy = false,
-  opts = {
-    skip_filetypes = {},
-  },
-  enabled = true,
-}
-
--- TODO: replace it with native feature of minimum lines above and under 
